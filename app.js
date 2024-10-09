5141db2bd1a996ccfd9987920ae6e87af9ceb45d
UNOWN7 <115024085+UNOWN7@users.noreply.github.com>
2024-10-09 18:14:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,13 @@ db.sequelize.sync({ alter: true }) // 'alter: true' adjusts tables to match mode
   });
 
 
-
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 
   const PORT = process.env.PORT || 3000;
